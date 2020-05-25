b193efae5d22da168d06c944b840c0fcbab38699
Pawel Paszki <pawelpaszki@gmail.com>
2020-05-25 19:55:10 +0100

    added skeleton and first button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ControlComponent } from './control/control.component';
+import { StatsComponent } from './stats/stats.component';
+import { FrameComponent } from './frame/frame.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ControlComponent,
+    StatsComponent,
+    FrameComponent
   ],
   imports: [
     BrowserModule
